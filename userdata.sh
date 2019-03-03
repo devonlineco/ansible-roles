9e9e4783aac21006ce0fe0e35bed324642a8a039
Andrei Miulescu <lusu777@gmail.com>
2019-03-03 10:59:33 +0000

    vnc

--- userdata.sh
+++ userdata.sh
@@ -1,21 +1,10 @@
 #!/usr/bin/env bash
 
 sudo apt-get update && sudo apt-get install \
-    build-essential \
-    libssl-dev \
-    libffi-dev \
-    python-dev \
-    python-pip \
-    python-setuptools \
-    python-virtualenv -y
+    ansible 
 
-git clone git@github.com:devbox-hero/ansible-roles.git
+git clone https://github.com/devbox-hero/ansible-roles.git
 
 cd ansible-roles/
 
-python -m virtualenv --python=`which python3` env &&
-        source env/bin/activate &&
-        python -m pip install -U pip virtualenv &&
-        python -m pip install -r requirements.txt
-  
-./devbox
+ansible-playbook main.yml
